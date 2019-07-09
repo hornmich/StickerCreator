aa096deb4c16296feab4609bbde038a5ec99d717
Micha Horn <michal@apartman.cz>
2019-07-09 23:38:55 +0200

    Move StickerPage to separate module

--- Sticker_creator/sticker.py
+++ Sticker_creator/sticker.py
@@ -25,55 +25,7 @@ Created on 15. 6. 2019
 
 import os
 from Sticker_creator import Order
-
-
-class StickerPage:
-    '''
-    Sticker page constructor.
-    '''
-    tempatePage = ''
-    TIMGPREFIX='ST_IMG_'
-    TLBLPREFIX='ST_LBL_'
-    
-    def __init__(self, templateFile) :
-        '''
-        Create new sticker constructor by using given HTML template.
-        '''
-        with open(templateFile) as template:
-            self.tempatePage = template.read();
-            template.close()
-            
-    def putSticker(self, index, item):
-        '''
-        Insert sticker on a position given by index.
-        '''
-        image=item[1]
-        label=item[0]
-        index_str='{0:02d}'.format(index)
-        timgkey=self.TIMGPREFIX+index_str
-        tlblkey=self.TLBLPREFIX+index_str
-        self.tempatePage = self.tempatePage.replace(timgkey, image)
-        self.tempatePage = self.tempatePage.replace(tlblkey, label)
-        
-    def clearSticker(self, index):
-        '''
-        Remove sticker from position given by index.
-        '''
-        image=''
-        label=''
-        index_str='{0:02d}'.format(index)
-        timgkey=self.TIMGPREFIX+index_str
-        tlblkey=self.TLBLPREFIX+index_str
-        self.tempatePage = self.tempatePage.replace(timgkey, image)
-        self.tempatePage = self.tempatePage.replace(tlblkey, label)
-        
-    def save(self, fileName):
-        '''
-        Save this stickers to a file.
-        '''
-        with open(fileName, 'w') as result:
-            result.write(self.tempatePage)
-            result.close()
+from Sticker_creator import StickerPage
 
 class ConsoleStickerCreator:
     def loadOrderProducts(self):
